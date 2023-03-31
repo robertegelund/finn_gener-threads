b91f91c7425570a144c830492005eb7bfb95b5b1
Robert Egelund <robert.egelund@outlook.com>
2023-03-31 16:25:34 +0200

    Legger til flere kommentarer i Oblig5Del1.java

--- Oblig5Del1.java
+++ Oblig5Del1.java
@@ -5,16 +5,21 @@ import java.util.Scanner;
 
 public class Oblig5Del1 {
     public static void main(String[] args) {
-        Scanner sc = null;
+        if(args.length == 0) {
+            System.out.println("[ERROR] Du maa sende inn mappestien som argument til programmet: " + 
+            "java Oblig5Del1 mappesti");
+            System.exit(1);
+        }
         
+        Scanner sc = null;
         try {
             sc = new Scanner(new File(args[0] + "/metadata.csv")); 
         } catch(FileNotFoundException e) {
             System.out.println("[ERROR] Finner ikke filen: metadata.csv");
         }
 
+        // Oppretter subsekvensregister, leser inn filer og legger inn subsekvenser
         SubsekvensRegister subSekReg = new SubsekvensRegister();
-
         while(sc.hasNextLine()) {
             subSekReg.settInn(SubsekvensRegister.lesFil(args[0] + "/" + sc.nextLine()));
         }
@@ -30,7 +35,7 @@ public class Oblig5Del1 {
             subSekReg.settInn(hMapSammen);
         }
 
-        // Skriver ut den mest frekvente subsekvensen i siste HashMap
+        // Finner og skriver ut den mest frekvente subsekvensen i siste HashMap
         int hoeyesteFrekvens = 0;
         Subsekvens mestFrekvente = null;
         for(String noekkel : hMapSammen.keySet()) {
@@ -40,6 +45,7 @@ public class Oblig5Del1 {
                 mestFrekvente = subsek;
             }
         }
+
         System.out.println(mestFrekvente);
     } 
 }
